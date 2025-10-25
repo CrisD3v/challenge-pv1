123b084ce9f14b5a1860394b120d6eb10e0e6873
Cristian Londono <crisdevenginer@gmail.com>
2025-10-25 12:14:18 -0500

    refactor(main): adjust settings
    - Add logger to show server
    
    - Add enable cors
    
    - Add global prefix
    
    Refs: #4

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+
+  Logger.log(`🚀 Servidor corriendo en: http://localhost:${PORT}/api`);
 }
 bootstrap();
